447e339fb0ecafbd2e3800a8f8560755ba04f148
murakamj <murakamj@Ubuntu.myguest.virtualbox.org>
2024-02-24 15:54:46 +0300

    i did the last task

--- script.bash
+++ script.bash
@@ -10,17 +10,20 @@ ip3=0
 ip4=0
 for oct in "${SPLITIP[@]}";
 do
-    i=$((i+1))
-    if [[ i -eq 1 ]]; then
-    	ip1=$oct
-    elif [[ i -eq 2 ]]; then 
-    	ip2=$oct
-    elif [[ i -eq 3 ]]; then
-    	ip3=$oct
-    else
-    	ip4=$oct
-    	printf "%08d\n" $(bc <<<"obase=2; $ip1; $ip2; $ip3; $ip4") | paste -sd.;
-    fi
+	if (( $oct <= 256 )); then
+		i=$((i+1))
+		if [[ i -eq 1 ]]; then
+			ip1=$oct
+		elif [[ i -eq 2 ]]; then 
+			ip2=$oct
+		elif [[ i -eq 3 ]]; then
+			ip3=$oct
+		else
+			ip4=$oct
+			i=0
+			printf "%08d\n" $(bc <<<"obase=2; $ip1; $ip2; $ip3; $ip4") | paste -sd.;
+		fi
+	fi
 
 done
 echo
